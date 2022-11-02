263d0e2479430a6d06aecdb9e042a8166b16c920
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-01 23:51:51 -0400

    import className library and apply it for the button props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,11 +4,14 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const setDay = (sample1) => {
-    console.log("this prop was clicked");
-  };
+  const dayClass = classNames("day-list", {
+    "day-list__item": props.name,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
