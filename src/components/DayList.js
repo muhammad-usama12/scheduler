aaf816e15781336ab7289002fd774f2c2f4562d7
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-02 17:34:51 -0400

    create DayList component to render DayListItems component and transfer data from storybook to test in browser

---
+++ src/components/DayList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "components/DayListItem";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  const parsedDayList = props.days.map((day) => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
+  return <ul>{parsedDayList}</ul>;
+}
