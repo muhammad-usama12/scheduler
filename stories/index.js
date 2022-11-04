2c4ec9b4ba66e9899b25efe6e21b4d9caed59743
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-04 10:57:54 -0400

    add appointment empty and booked stories

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -163,4 +163,20 @@ storiesOf("Appointment", module)
       interviewer={2}
       interviewers={interviewers}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
