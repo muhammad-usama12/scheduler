95f2e7ca05de06566752e858098172b77c57ae9a
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-09 22:13:02 -0500

    create update function which dynamically updates the spots on the client side as well as the server side

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,6 +10,63 @@ export default function useApplicationData() {
 
   const setDay = (day) => setState({ ...state, day });
 
+  function updateSpot(interview) {
+    // pick day based on user selection
+    const day = state.days.find((indvDay) => indvDay.name === state.day);
+    // update spot based on interview
+    if (interview) {
+      day.spots -= 1;
+    } else {
+      day.spots += 1;
+    }
+    // create new day data based on update from the user
+    const days = [...state.days];
+    days[day.id - 1] = day;
+    return days;
+  }
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    // assign and call update function to update day result in the server
+    const days = updateSpot(appointment.interview);
+    return axios.put(`api/appointments/${id}`, appointment).then((result) => {
+      setState({
+        ...state,
+        days,
+        appointments,
+      });
+    });
+  }
+
+  function deleteInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    // assign and call update function to update day result in the server
+    const days = updateSpot(appointment.interview);
+    return axios
+      .delete(`api/appointments/${id}`, appointment)
+      .then((result) => {
+        setState({
+          ...state,
+          days,
+          appointments,
+        });
+      });
+  }
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
@@ -26,46 +83,6 @@ export default function useApplicationData() {
       })
       .catch((e) => console.log(e));
   }, []);
-
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-    return axios.put(`api/appointments/${id}`, appointment).then((result) => {
-      console.log("axios.put result:", result);
-      setState({
-        ...state,
-        appointments,
-      });
-    });
-  }
-
-  function deleteInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    return axios
-      .delete(`api/appointments/${id}`, appointment)
-      .then((result) => {
-        console.log("axios.delete result:", result);
-        setState({
-          ...state,
-          appointments,
-        });
-      });
-  }
 
   return {
     state,
