95d5b117bae7aba3d8483645cc2d1764b79b50d9
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-21 01:05:57 -0500

    add newHistroy variable  to prevent state mutations directly

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,19 +4,30 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+
+
   function transition(transitionMode, replace = false) {
+    let newHistory = [...history]
     if (replace) {
-      history.pop();
+      newHistory = newHistory.slice(0, -1)
+      newHistory = [...newHistory, transitionMode]
+      setHistory(newHistory);
+      setMode(transitionMode);
     }
-    history.push(transitionMode);
-    setHistory(history);
-    setMode(transitionMode);
+    else {
+      setHistory(prev => (
+        [...prev, transitionMode]
+      ));
+      setMode(transitionMode)
+    }
   }
 
   function back() {
     if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
+      let newHistory= [...history];
+      newHistory.pop();
+      setHistory(newHistory)
+      setMode(newHistory[newHistory.length - 1]);
     }
   }
   return { mode, transition, back };
