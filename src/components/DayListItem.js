a8e0999991b9f32407214a310f59b255f80f4c88
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-11-21 01:08:35 -0500

    fix UI for dayListItem, day is highlighted white when selected

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,13 @@
 import React from "react";
 import classNames from "classnames";
-
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
   const formatSpots = (spots) => {
     if (spots  === 1) {
       return "1 spot remaining";
@@ -13,15 +17,8 @@ export default function DayListItem(props) {
       return `${spots} spots remaining`;
     }
   };
-
-  const dayClass = classNames("day-list", {
-    "day-list__item": props.name,
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0,
-  });
-
   return (
-    <li data-testid='day' className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} data-testid='day'>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
