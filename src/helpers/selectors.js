99bc167354ea8e03692f8c1ca9627862532da3c2
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-12-11 02:47:44 -0500

    fix conflicts

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -37,25 +37,3 @@ export function getInterviewersForDay(state, day) {
   );
   return interviewCheck;
 }
-
-export function addComment() {
-  return axios
-    .post("/api/comments/new", {
-      text: text,
-    })
-    .then((res) => {
-      setState((prev) => ({ ...prev, comments: [...prev.comments, res.data] }));
-      console.log("res from commentForm.js: ", res);
-    });
-}
-export function addPost() {
-  return axios
-    .post("/api/posts/new", {
-      text: text,
-      show: show,
-    })
-    .then((res) => {
-      setState((prev) => ({ ...prev, posts: [...prev.posts, res.data] }));
-      console.log("res from write.js: ", res);
-    });
-}
