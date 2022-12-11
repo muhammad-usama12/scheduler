cbf3b93419f22fbf304d48966d886a4c9bc6d22f
Muhammad Usama <muhammad.usama12@hotmail.com>
2022-12-11 02:27:47 -0500

    update last commit to ensure all tests pass (validate circleci)

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,16 +1,16 @@
-// export function getAppointmentsForDay(state, day) {
-//   const result = [];
-//   const selectedDay = state.days.filter((dayName) => dayName.name === day)[0];
+export function getAppointmentsForDay(state, day) {
+  const result = [];
+  const selectedDay = state.days.filter((dayName) => dayName.name === day)[0];
 
-//   if (!selectedDay) {
-//     return result;
-//   }
-//   const appointmentCheck = selectedDay.appointments.map(
-//     (appointment) => state.appointments[appointment]
-//   );
+  if (!selectedDay) {
+    return result;
+  }
+  const appointmentCheck = selectedDay.appointments.map(
+    (appointment) => state.appointments[appointment]
+  );
 
-//   return appointmentCheck;
-// }
+  return appointmentCheck;
+}
 
 export function getInterview(state, interview) {
   const result = {};
